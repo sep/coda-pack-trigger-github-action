fc59a7fdfa422ffcf12c9cf0acedd50cdc3d5c9e
Jon Fuller <jcfuller@sep.com>
2022-05-27 10:06:13 -0400

    initial commit

---
+++ pack.ts
@@ -0,0 +1,55 @@
+import * as coda from "@codahq/packs-sdk";
+
+export const pack = coda.newPack();
+
+pack.addNetworkDomain("github.com");
+
+pack.setUserAuthentication({
+  type: coda.AuthenticationType.HeaderBearerToken,
+
+  // Determines the name of the GitHub account that was connected.
+  getConnectionName: async function (context) {
+    let response = await context.fetcher.fetch({
+      method: "GET",
+      url: "https://api.github.com/user",
+    });
+    return response.body.login;
+  },
+});
+
+// A formula to trigger a github action.
+pack.addFormula({
+  name: "GithubAction",
+  description: "Trigger a Github Action Workflow.",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "repo_with_owner",
+      description: "The name of the repo (e.g. org/repo).",
+      suggestedValue: "sep/SEPDataWarehouse"
+    }),
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "event",
+      description: "The name of the event to trigger.",
+      suggestedValue: "coda-event"
+    }),
+  ],
+  resultType: coda.ValueType.String,
+  isAction: true,
+
+  execute: async function ([repo_with_owner, event], context) {
+    let response = await context.fetcher.fetch({
+      url: `https://api.github.com/repos/${repo_with_owner}/dispatches`,
+      method: "POST",
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+      },
+      body: JSON.stringify({
+        event_type: event,
+      }),
+    });
+
+    return response.status == 204 ? "✔️" : "❌";
+  },
+});
